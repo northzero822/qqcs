6e4d29f5610c5d1b7f9d77ed4bba36f9ebc81d3c
okita <t-okita@netoffice.obisan.co.jp>
2021-01-25 13:11:29 +0900

    insert, update, select, delete test

--- qqcs/MainWindow.xaml.cs
+++ qqcs/MainWindow.xaml.cs
@@ -44,28 +44,58 @@ namespace qqcs
                 MessageBox.Show("接続しました");
                 InsertTest(connection);
                 SelectTest(connection);
+                UpdateTest(connection);
+                SelectTest(connection);
+                DeleteTest(connection);
 
             }
 
         }
         private void InsertTest(SqlConnection connection)
         {
-            string sql = "";
-            sql += "SELECT * FROM Mユーザー情報";
+            string sql = "INSERT INTO M地区 VALUES(99,'99',99,99)";
             using (SqlCommand command = new SqlCommand(sql, connection))
             {
-
+                int rowsAffected = command.ExecuteNonQuery();
+                MessageBox.Show(rowsAffected.ToString() + " 行 挿入されました。");
             }
         }
 
         private void SelectTest(SqlConnection connection)
         {
-            string sql = "";
-            sql += "SELECT * FROM Mユーザー情報";
+            int i = 0;
+            string sql = "SELECT * FROM M地区 WHERE 地区コード=99";
             using (SqlCommand command = new SqlCommand(sql, connection))
             {
-
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        i = (int)reader["地区コード"];
+                        MessageBox.Show(i.ToString() + reader["地区名"]);
+                    }
+                }
             }
+        }
+
+        private void UpdateTest(SqlConnection connection)
+        {
+            string sql = "UPDATE M地区 SET 地区名='100' WHERE 地区コード=99";
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                int rowsAffected = command.ExecuteNonQuery();
+                MessageBox.Show(rowsAffected.ToString() + "更新しました");
+            }
+        }
+        private void DeleteTest(SqlConnection connection)
+        {
+            string sql = "DELETE M地区 WHERE 地区コード=99";
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                int rowsAffected = command.ExecuteNonQuery();
+                MessageBox.Show(rowsAffected.ToString() + " 行 削除されました。");
+            }
+
         }
     }
 }
