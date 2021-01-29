2ec70c3ffcc975e3754941b8c4410ef587282986
okita <t-okita@netoffice.obisan.co.jp>
2021-01-29 14:50:21 +0900

    xmlから設定読み込み
    コンボボックスからクエリを選んで、SQLを読み込んで結果表示

--- qqcs/MainWindow.xaml.cs
+++ qqcs/MainWindow.xaml.cs
@@ -67,70 +67,16 @@ namespace qqcs
            
             this.Connection.Open();
 
-            //InsertTest();
-
-            //SelectTest(connection);
-            //UpdateTest(connection);
-            //SelectTest(connection);
-            //DeleteTest(connection);
-
-
-        }
-        private void InsertTest()
-        {
-            string sql = "INSERT INTO M地区 VALUES(99,'99',99,99)";
-            using (SqlCommand command = new SqlCommand(sql, this.Connection))
-            {
-                int rowsAffected = command.ExecuteNonQuery();
-                MessageBox.Show(rowsAffected.ToString() + " 行 挿入されました。");
-            }
-        }
-
-        private void SelectTest(SqlConnection connection)
-        {
-            int i = 0;
-            string sql = "SELECT * FROM D売上入金 WHERE 削除区分=0 AND 伝票日付>=20200101 AND データ区分=1";
-            using (SqlCommand command = new SqlCommand(sql, connection))
-            {
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        i = (int)reader["地区コード"];
-                        MessageBox.Show(i.ToString() + reader["地区名"]);
-                    }
-                }
-            }
-        }
-
-        private void UpdateTest(SqlConnection connection)
-        {
-            string sql = "UPDATE M地区 SET 地区名='100' WHERE 地区コード=99";
-            using (SqlCommand command = new SqlCommand(sql, connection))
-            {
-                int rowsAffected = command.ExecuteNonQuery();
-                MessageBox.Show(rowsAffected.ToString() + "更新しました");
-            }
-        }
-        private void DeleteTest(SqlConnection connection)
-        {
-            string sql = "DELETE M地区 WHERE 地区コード=99";
-            using (SqlCommand command = new SqlCommand(sql, connection))
-            {
-                int rowsAffected = command.ExecuteNonQuery();
-                MessageBox.Show(rowsAffected.ToString() + " 行 削除されました。");
-            }
-
         }
 
         private void Run_Click(object sender, RoutedEventArgs e)
         {
             this.dt = new DataTable();
 
-            var query = (from p in this.Xml.Elements("Query") where p.Attribute("id").Value == "0" select p).First();
-            MessageBox.Show(query.Element("Name").Value);
+            var id = this.QueryCombobox.SelectedIndex;
 
-            string sql = "SELECT * FROM D売上入金 WHERE 削除区分=0 AND 伝票日付>=20200101 AND データ区分=1";
+            var query = (from p in this.Xml.Elements("Query") where p.Attribute("id").Value == id.ToString() select p).First();
+            string sql = query.Element("SQL").Value;
             using (SqlCommand command = new SqlCommand(sql, this.Connection))
             {
                 var addapter = new SqlDataAdapter(command);
